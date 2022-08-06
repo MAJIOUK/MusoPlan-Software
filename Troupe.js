9b31a7e2dfd5ff675e05c99b23b026bf15a1da44
maj10 <radewale10@gmail.com>
2022-08-06 20:33:00 +1000

    included troupeDetails() method in Troupe class in file Troupe.js

--- Troupe.js
+++ Troupe.js
@@ -93,6 +93,68 @@ class Troupe {
         console.log("==============================================================================");
     }
 
+    // print Troupe details
+    troupeDetails() {
+        //initializing variables used in the loops below
+        let troupeRate = 0;
+        let guitaristCount = 0;
+        let bassistCount = 0;
+        let percussionistCount = 0; 
+        let flautistCount = 0;
+        let addedMusicians = new Set();
+
+        //print troupe name and genre to console
+        console.log("===============================================================");
+        console.log("Troupe Name: " + this.troupe_name);
+        console.log("Troupe Genre: " + this.music_genre);
+        
+        //loop to aggregate the names of added musicians to a troupe
+        for (let j = 0; j < this.troupeMusician.length; j++) {
+            
+            addedMusicians = addedMusicians.add(this.troupeMusician[j].musicianName)              
+            
+        }
+        //print names of musician in any troupe to console
+        console.log("Names of musicians in the troupe: " +Array.from(addedMusicians));
+
+        //loop to introduce each musician, rate, instrumentalist count per loop
+        for (let j = 0; j < this.troupeMusician.length; j++) {
+
+            console.log("Musician Intro: " + this.troupeMusician[j].musicianIntro);
+            console.log("Interesting fact: " + this.troupeMusician[j].intFact);
+            
+            troupeRate = troupeRate+this.troupeMusician[j].hourlyRate
+
+
+            if (this.troupeMusician[j].instrumentalist=="guitarist"){
+                let j = 1;
+                guitaristCount = guitaristCount + j
+            }
+            if (this.troupeMusician[j].instrumentalist=="bassist"){
+                let j = 1;
+                bassistCount = bassistCount + j
+            }
+            if (this.troupeMusician[j].instrumentalist=="percussionist"){
+                let j = 1;
+                percussionistCount = percussionistCount + j
+            }
+            if (this.troupeMusician[j].instrumentalist=="flautist"){
+                let j = 1;
+                flautistCount = flautistCount + j
+            }              
+        }
+        
+        console.log("No of Guitarist: " +guitaristCount);
+        console.log("No of Bassist: " +bassistCount);
+        console.log("No of Percussionist: " +percussionistCount);
+        console.log("No of Flautist: " +flautistCount);
+        console.log("Total no. of musicians: " +addedMusicians.size);
+        console.log("Booking Duration (hrs): " + this.troupe_duration);
+        console.log("Troupe hourly rate (AUD): "+troupeRate);
+        console.log("Cost of troupe deployment (AUD): "+troupeRate*this.troupe_duration);
+        console.log("==============================================================================");
+    }
+
 }
         
     
