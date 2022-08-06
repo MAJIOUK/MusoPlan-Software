458e1bd832ec1da0f18ac376d80e5c62a324ad63
maj10 <radewale10@gmail.com>
2022-08-06 20:50:13 +1000

    created class percussionist in file percussionist.js

---
+++ percussionist.js
@@ -0,0 +1,14 @@
+//defining a percussionist class
+class percussionist{
+    constructor()
+    {
+
+    }
+    displayInterestingFact()
+    {
+        return("Me drum");
+        
+    }
+}
+
+module.exports={percussionist};
